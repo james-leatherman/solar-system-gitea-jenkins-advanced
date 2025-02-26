a282ba74360cee8d374110dd28bebcfbe46b2bcd
James Leatherman <james.leatherman@gmail.com>
2025-02-26 13:03:51 +0000

    Remove deprecated mongoose options

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ app.use(cors());
         await mongoose.connect(process.env.MONGO_URI, {
             user: process.env.MONGO_USERNAME,
             pass: process.env.MONGO_PASSWORD,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
         });
         console.log("MongoDB Connection Successful");
     } catch (err) {
